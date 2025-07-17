850d1b1f1cddb9b473f00d74c9f571183f18df34
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2025-07-17 12:54:23 -0700

    Removed outdated code

--- omg/sampler/distributions.py
+++ omg/sampler/distributions.py
@@ -5,25 +5,6 @@ import scipy
 import torch
 from torch.distributions import LogNormal
 
-# TODO: !!!Highly WIP!!! from MatterGen for lattice sampling. Not sure it actually works like they say it does!
-'''
-class NDependentScaledNormal:
-    """
-    Samples a scaled normal distribution which is scaled according to n, with c and v fixed by the given dataset
-    """
-    def __init__(self, c):
-        self.c = c
-        self.rng = np.random.default_rng()
-
-    def __call__(self, n, size):
-        mean = (n * self.c) ** (1/3)
-        stdev = mean / 8
-        val = self.rng.normal(mean, stdev, size)
-        l = [val, val, val] * np.identity(3)
-        print (l)
-        return l
-'''
-
 
 class NDependentGamma:
     def __init__(self, a, loc, scale):
