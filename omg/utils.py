e4bdc807cc9df8c0b0d714e04d8beb5250ffc924
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2025-07-17 12:57:51 -0700

    Copied over necessary analysis code from DiffCSP

--- omg/utils.py
+++ omg/utils.py
@@ -8,7 +8,7 @@ from lightning.pytorch.loggers.wandb import WandbLogger
 import matplotlib.pyplot as plt
 import torch
 from torch_geometric.data import Data
-
+import numpy as np
 
 class DataField(Enum):
     pos = auto()
@@ -125,3 +125,68 @@ class OMGLearningRateFinder(LearningRateFinder):
         else:
             directory = trainer.logger.log_dir
         plt.savefig(directory + "/lr-finder.png")
+
+
+# Copied from https://github.com/jiaor17/DiffCSP/blob/7121d159826efa2ba9500bf299250d96da37f146/diffcsp/common/data_utils.py
+class StandardScaler:
+
+    """A :class:`StandardScaler` normalizes the features of a dataset.
+    When it is fit on a dataset, the :class:`StandardScaler` learns the
+        mean and standard deviation across the 0th axis.
+    When transforming a dataset, the :class:`StandardScaler` subtracts the
+        means and divides by the standard deviations.
+    """
+
+    def __init__(self, means=None, stds=None, replace_nan_token=None):
+        """
+        :param means: An optional 1D numpy array of precomputed means.
+        :param stds: An optional 1D numpy array of precomputed standard deviations.
+        :param replace_nan_token: A token to use to replace NaN entries in the features.
+        """
+        self.means = means
+        self.stds = stds
+        self.replace_nan_token = replace_nan_token
+
+    def fit(self, X):
+        """
+        Learns means and standard deviations across the 0th axis of the data :code:`X`.
+        :param X: A list of lists of floats (or None).
+        :return: The fitted :class:`StandardScaler` (self).
+        """
+        X = np.array(X).astype(float)
+        self.means = np.nanmean(X, axis=0)
+        self.stds = np.nanstd(X, axis=0)
+        self.means = np.where(np.isnan(self.means),
+                              np.zeros(self.means.shape), self.means)
+        self.stds = np.where(np.isnan(self.stds),
+                             np.ones(self.stds.shape), self.stds)
+        self.stds = np.where(self.stds == 0, np.ones(
+            self.stds.shape), self.stds)
+
+        return self
+
+    def transform(self, X):
+        """
+        Transforms the data by subtracting the means and dividing by the standard deviations.
+        :param X: A list of lists of floats (or None).
+        :return: The transformed data with NaNs replaced by :code:`self.replace_nan_token`.
+        """
+        X = np.array(X).astype(float)
+        transformed_with_nan = (X - self.means) / self.stds
+        transformed_with_none = np.where(
+            np.isnan(transformed_with_nan), self.replace_nan_token, transformed_with_nan)
+
+        return transformed_with_none
+
+    def inverse_transform(self, X):
+        """
+        Performs the inverse transformation by multiplying by the standard deviations and adding the means.
+        :param X: A list of lists of floats.
+        :return: The inverse transformed data with NaNs replaced by :code:`self.replace_nan_token`.
+        """
+        X = np.array(X).astype(float)
+        transformed_with_nan = X * self.stds + self.means
+        transformed_with_none = np.where(
+            np.isnan(transformed_with_nan), self.replace_nan_token, transformed_with_nan)
+
+        return transformed_with_none
